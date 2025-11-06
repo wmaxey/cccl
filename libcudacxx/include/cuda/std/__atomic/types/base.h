84baf4e16c7d62b1aa825e0fd1641fa02d992e0b
Wesley Maxey <wesley.maxey@gmail.com>
2025-11-06 21:00:38 +0000

    Use atomic order conversion instead of static cast.

--- libcudacxx/include/cuda/std/__atomic/types/base.h
+++ libcudacxx/include/cuda/std/__atomic/types/base.h
@@ -22,6 +22,7 @@
 #endif // no system header
 
 #include <cuda/std/__atomic/backends.h>
+#include <cuda/std/__atomic/order.h>
 #include <cuda/std/__atomic/types/common.h>
 #include <cuda/std/__type_traits/is_trivially_copyable.h>
 
@@ -69,7 +70,7 @@ _CCCL_HOST_DEVICE inline void __atomic_thread_fence_dispatch(memory_order __orde
 {
   NV_DISPATCH_TARGET(
     NV_IS_DEVICE,
-    (__atomic_thread_fence_cuda(static_cast<__memory_order_underlying_t>(__order), __thread_scope_system_tag());),
+    (__atomic_thread_fence_cuda(::cuda::std::__atomic_order_to_int(__order), __thread_scope_system_tag());),
     NV_IS_HOST,
     (__atomic_thread_fence_host(__order);))
 }
@@ -77,7 +78,7 @@ _CCCL_HOST_DEVICE inline void __atomic_thread_fence_dispatch(memory_order __orde
 _CCCL_HOST_DEVICE inline void __atomic_signal_fence_dispatch(memory_order __order)
 {
   NV_DISPATCH_TARGET(NV_IS_DEVICE,
-                     (__atomic_signal_fence_cuda(static_cast<__memory_order_underlying_t>(__order));),
+                     (__atomic_signal_fence_cuda(::cuda::std::__atomic_order_to_int(__order));),
                      NV_IS_HOST,
                      (__atomic_signal_fence_host(__order);))
 }
@@ -91,22 +92,21 @@ _CCCL_HOST_DEVICE inline void __atomic_init_dispatch(_Sto* __a, _Up __val)
 template <typename _Sto, typename _Up, typename _Sco, __atomic_storage_is_base<_Sto> = 0>
 _CCCL_HOST_DEVICE inline void __atomic_store_dispatch(_Sto* __a, _Up __val, memory_order __order, _Sco = {})
 {
-  NV_DISPATCH_TARGET(
-    NV_IS_DEVICE,
-    (__atomic_store_n_cuda(__a->get(), __val, static_cast<__memory_order_underlying_t>(__order), _Sco{});),
-    NV_IS_HOST,
-    (__atomic_store_host(__a->get(), __val, __order);))
+  NV_DISPATCH_TARGET(NV_IS_DEVICE,
+                     (__atomic_store_n_cuda(__a->get(), __val, ::cuda::std::__atomic_order_to_int(__order), _Sco{});),
+                     NV_IS_HOST,
+                     (__atomic_store_host(__a->get(), __val, __order);))
 }
 
 template <typename _Sto, typename _Sco, __atomic_storage_is_base<_Sto> = 0>
 _CCCL_HOST_DEVICE inline auto __atomic_load_dispatch(const _Sto* __a, memory_order __order, _Sco = {})
   -> __atomic_underlying_t<_Sto>
 {
-  NV_DISPATCH_TARGET(
-    NV_IS_DEVICE,
-    (return __atomic_load_n_cuda(__a->get(), static_cast<__memory_order_underlying_t>(__order), _Sco{});),
-    NV_IS_HOST,
-    (return __atomic_load_host(__a->get(), __order);))
+  NV_DISPATCH_TARGET(NV_IS_DEVICE,
+                     (return __atomic_load_n_cuda(__a->get(), ::cuda::std::__atomic_order_to_int(__order), _Sco{});),
+                     NV_IS_HOST,
+                     (return __atomic_load_host(__a->get(), __order);))
+  _CCCL_UNREACHABLE();
 }
 
 template <typename _Sto, typename _Up, typename _Sco, __atomic_storage_is_base<_Sto> = 0>
@@ -115,7 +115,7 @@ _CCCL_HOST_DEVICE inline auto __atomic_exchange_dispatch(_Sto* __a, _Up __value,
 {
   NV_DISPATCH_TARGET(
     NV_IS_DEVICE,
-    (return __atomic_exchange_n_cuda(__a->get(), __value, static_cast<__memory_order_underlying_t>(__order), _Sco{});),
+    (return __atomic_exchange_n_cuda(__a->get(), __value, ::cuda::std::__atomic_order_to_int(__order), _Sco{});),
     NV_IS_HOST,
     (return __atomic_exchange_host(__a->get(), __value, __order);))
 }
@@ -132,8 +132,8 @@ _CCCL_HOST_DEVICE inline bool __atomic_compare_exchange_strong_dispatch(
        __expected,
        __val,
        false,
-       static_cast<__memory_order_underlying_t>(__success),
-       static_cast<__memory_order_underlying_t>(__failure),
+       ::cuda::std::__atomic_order_to_int(__success),
+       ::cuda::std::__atomic_order_to_int(__failure),
        _Sco{});),
     NV_IS_HOST,
     (__result = __atomic_compare_exchange_strong_host(__a->get(), __expected, __val, __success, __failure);))
@@ -152,8 +152,8 @@ _CCCL_HOST_DEVICE inline bool __atomic_compare_exchange_weak_dispatch(
        __expected,
        __val,
        true,
-       static_cast<__memory_order_underlying_t>(__success),
-       static_cast<__memory_order_underlying_t>(__failure),
+       ::cuda::std::__atomic_order_to_int(__success),
+       ::cuda::std::__atomic_order_to_int(__failure),
        _Sco{});),
     NV_IS_HOST,
     (__result = __atomic_compare_exchange_weak_host(__a->get(), __expected, __val, __success, __failure);))
@@ -166,7 +166,7 @@ _CCCL_HOST_DEVICE inline auto __atomic_fetch_add_dispatch(_Sto* __a, _Up __delta
 {
   NV_DISPATCH_TARGET(
     NV_IS_DEVICE,
-    (return __atomic_fetch_add_cuda(__a->get(), __delta, static_cast<__memory_order_underlying_t>(__order), _Sco{});),
+    (return __atomic_fetch_add_cuda(__a->get(), __delta, ::cuda::std::__atomic_order_to_int(__order), _Sco{});),
     NV_IS_HOST,
     (return __atomic_fetch_add_host(__a->get(), __delta, __order);))
 }
@@ -177,7 +177,7 @@ _CCCL_HOST_DEVICE inline auto __atomic_fetch_sub_dispatch(_Sto* __a, _Up __delta
 {
   NV_DISPATCH_TARGET(
     NV_IS_DEVICE,
-    (return __atomic_fetch_sub_cuda(__a->get(), __delta, static_cast<__memory_order_underlying_t>(__order), _Sco{});),
+    (return __atomic_fetch_sub_cuda(__a->get(), __delta, ::cuda::std::__atomic_order_to_int(__order), _Sco{});),
     NV_IS_HOST,
     (return __atomic_fetch_sub_host(__a->get(), __delta, __order);))
 }
@@ -188,7 +188,7 @@ _CCCL_HOST_DEVICE inline auto __atomic_fetch_and_dispatch(_Sto* __a, _Up __patte
 {
   NV_DISPATCH_TARGET(
     NV_IS_DEVICE,
-    (return __atomic_fetch_and_cuda(__a->get(), __pattern, static_cast<__memory_order_underlying_t>(__order), _Sco{});),
+    (return __atomic_fetch_and_cuda(__a->get(), __pattern, ::cuda::std::__atomic_order_to_int(__order), _Sco{});),
     NV_IS_HOST,
     (return __atomic_fetch_and_host(__a->get(), __pattern, __order);))
 }
@@ -199,7 +199,7 @@ _CCCL_HOST_DEVICE inline auto __atomic_fetch_or_dispatch(_Sto* __a, _Up __patter
 {
   NV_DISPATCH_TARGET(
     NV_IS_DEVICE,
-    (return __atomic_fetch_or_cuda(__a->get(), __pattern, static_cast<__memory_order_underlying_t>(__order), _Sco{});),
+    (return __atomic_fetch_or_cuda(__a->get(), __pattern, ::cuda::std::__atomic_order_to_int(__order), _Sco{});),
     NV_IS_HOST,
     (return __atomic_fetch_or_host(__a->get(), __pattern, __order);))
 }
@@ -210,7 +210,7 @@ _CCCL_HOST_DEVICE inline auto __atomic_fetch_xor_dispatch(_Sto* __a, _Up __patte
 {
   NV_DISPATCH_TARGET(
     NV_IS_DEVICE,
-    (return __atomic_fetch_xor_cuda(__a->get(), __pattern, static_cast<__memory_order_underlying_t>(__order), _Sco{});),
+    (return __atomic_fetch_xor_cuda(__a->get(), __pattern, ::cuda::std::__atomic_order_to_int(__order), _Sco{});),
     NV_IS_HOST,
     (return __atomic_fetch_xor_host(__a->get(), __pattern, __order);))
 }
@@ -221,7 +221,7 @@ _CCCL_HOST_DEVICE inline auto __atomic_fetch_max_dispatch(_Sto* __a, _Up __val, 
 {
   NV_IF_TARGET(
     NV_IS_DEVICE,
-    (return __atomic_fetch_max_cuda(__a->get(), __val, static_cast<__memory_order_underlying_t>(__order), _Sco{});),
+    (return __atomic_fetch_max_cuda(__a->get(), __val, ::cuda::std::__atomic_order_to_int(__order), _Sco{});),
     (return __atomic_fetch_max_host(__a->get(), __val, __order);))
 }
 
@@ -231,7 +231,7 @@ _CCCL_HOST_DEVICE inline auto __atomic_fetch_min_dispatch(_Sto* __a, _Up __val, 
 {
   NV_IF_TARGET(
     NV_IS_DEVICE,
-    (return __atomic_fetch_min_cuda(__a->get(), __val, static_cast<__memory_order_underlying_t>(__order), _Sco{});),
+    (return __atomic_fetch_min_cuda(__a->get(), __val, ::cuda::std::__atomic_order_to_int(__order), _Sco{});),
     (return __atomic_fetch_min_host(__a->get(), __val, __order);))
 }
 
