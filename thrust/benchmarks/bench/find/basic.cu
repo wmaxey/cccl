e1329caabcd618ce77706c6ba50f20105da572aa
Dhruvil Mehta <68022411+dhruvilmehta@users.noreply.github.com>
2026-03-20 12:23:01 -0700

    Unified equal_to_value functions  (#8114)

--- thrust/benchmarks/bench/find/basic.cu
+++ thrust/benchmarks/bench/find/basic.cu
@@ -4,25 +4,11 @@
 #include <thrust/device_vector.h>
 #include <thrust/find.h>
 
+#include <cuda/functional>
 #include <cuda/memory_pool>
 #include <cuda/stream>
 
 #include "nvbench_helper.cuh"
-
-struct equal_to_val
-{
-  size_t val_;
-
-  constexpr equal_to_val(const size_t val) noexcept
-      : val_(val)
-  {}
-
-  template <class T>
-  __device__ constexpr bool operator()(const T& val) const noexcept
-  {
-    return val == val_;
-  }
-};
 
 template <typename T>
 static void basic(nvbench::state& state, nvbench::type_list<T>)
@@ -42,10 +28,10 @@ static void basic(nvbench::state& state, nvbench::type_list<T>)
 
   caching_allocator_t alloc{};
 
-  state.exec(nvbench::exec_tag::gpu | nvbench::exec_tag::no_batch | nvbench::exec_tag::sync,
-             [&](nvbench::launch& launch) {
-               do_not_optimize(thrust::find(policy(alloc, launch), dinput.begin(), dinput.end(), val));
-             });
+  state.exec(
+    nvbench::exec_tag::gpu | nvbench::exec_tag::no_batch | nvbench::exec_tag::sync, [&](nvbench::launch& launch) {
+      do_not_optimize(thrust::find(policy(alloc, launch), dinput.begin(), dinput.end(), cuda::equal_to_value{val}));
+    });
 }
 
 NVBENCH_BENCH_TYPES(basic, NVBENCH_TYPE_AXES(fundamental_types))
