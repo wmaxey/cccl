26480aa6adcf9875cd351322da64d57a92e2e996
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-04-30 12:13:56 +0200

    Replace `detail::segmented_reduce::dispatch` by the public API (#8695)
    * Replace detail::segmented_reduce::dispatch by the public API
    
    Fixes: #7950
    
    * Fix
    
    * Refactor
    
    * Fix
    
    * Review
    
    * Update cub/benchmarks/bench/segmented_reduce/base.cuh
    
    Co-authored-by: Michael Schellenberger Costa <miscco@nvidia.com>
    
    * Update cub/benchmarks/bench/segmented_reduce/base.cuh
    
    Co-authored-by: Michael Schellenberger Costa <miscco@nvidia.com>
    
    ---------
    
    Co-authored-by: Michael Schellenberger Costa <miscco@nvidia.com>

--- cub/cub/device/dispatch/dispatch_segmented_reduce.cuh
+++ cub/cub/device/dispatch/dispatch_segmented_reduce.cuh
@@ -684,29 +684,25 @@ struct DeviceFixedSizeSegmentedReduceKernelSource
   }
 };
 
-template <
-  typename OverrideAccumT = use_default,
-  typename InputIteratorT,
-  typename OutputIteratorT,
-  typename OffsetT,
-  typename ReductionOpT,
-  typename InitT = non_void_value_t<OutputIteratorT, it_value_t<InputIteratorT>>,
-  typename AccumT =
-    decltype(select_segmented_accum_t<InputIteratorT, InitT, ReductionOpT>(static_cast<OverrideAccumT*>(nullptr))),
-  typename PolicySelector = policy_selector_from_hub<detail::segmented_reduce::policy_hub<AccumT, OffsetT, ReductionOpT>,
-                                                     AccumT,
-                                                     OffsetT,
-                                                     ReductionOpT>,
-  typename KernelSource = DeviceFixedSizeSegmentedReduceKernelSource<
-    PolicySelector,
-    InputIteratorT,
-    OutputIteratorT,
-    OffsetT,
-    ReductionOpT,
-    InitT,
-    AccumT>,
-  typename KernelLauncherFactory                                       = CUB_DETAIL_DEFAULT_KERNEL_LAUNCHER_FACTORY,
-  ::cuda::std::enable_if_t<::cuda::std::is_arithmetic_v<OffsetT>, int> = 0>
+template <typename OverrideAccumT = use_default,
+          typename InputIteratorT,
+          typename OutputIteratorT,
+          typename OffsetT,
+          typename ReductionOpT,
+          typename InitT          = non_void_value_t<OutputIteratorT, it_value_t<InputIteratorT>>,
+          typename AccumT         = decltype(select_segmented_accum_t<InputIteratorT, InitT, ReductionOpT>(
+            static_cast<OverrideAccumT*>(nullptr))),
+          typename PolicySelector = policy_selector_from_types<AccumT, OffsetT, ReductionOpT>,
+          typename KernelSource   = DeviceFixedSizeSegmentedReduceKernelSource<
+              PolicySelector,
+              InputIteratorT,
+              OutputIteratorT,
+              OffsetT,
+              ReductionOpT,
+              InitT,
+              AccumT>,
+          typename KernelLauncherFactory = CUB_DETAIL_DEFAULT_KERNEL_LAUNCHER_FACTORY,
+          ::cuda::std::enable_if_t<::cuda::std::is_arithmetic_v<OffsetT>, int> = 0>
 #if _CCCL_HAS_CONCEPTS()
   requires segmented_reduce_policy_selector<PolicySelector>
 #endif // _CCCL_HAS_CONCEPTS()
